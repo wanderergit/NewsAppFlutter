52da76f346d172ddf5be5bc222dc187ff7450624
Gokul Madhusudhan <gokul99.gm@gmail.com>
2020-10-24 18:05:21 +0530

    News App Static

---
+++ lib/views/home.dart
@@ -0,0 +1,161 @@
+import 'dart:ffi';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:newsapplication/helper/data.dart';
+import 'package:newsapplication/models/category_model.dart';
+import 'package:newsapplication/models/article_model.dart';
+import 'package:newsapplication/helper/news.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<CategoryModel> categories = new List<CategoryModel>();
+
+  List<ArticleModel> articles = new List<ArticleModel>();
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    categories = getCategories();
+    getNews();
+  }
+
+  getNews() async {
+    News newsClass = News();
+    await newsClass.getNews();
+    articles = newsClass.news;
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Daily"),
+              Text("News", style: TextStyle(color: Colors.blue))
+            ],
+          ),
+          elevation: 0.0,
+        ),
+        body: _loading
+            ? Center(child: Container(child: CircularProgressIndicator()))
+            : SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    children: <Widget>[
+                      //categories
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        height: 70,
+                        child: ListView.builder(
+                            itemCount: categories.length,
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return CategoryTile(
+                                imageUrl: categories[index].imageUrl,
+                                categoryName: categories[index].categoryName,
+                              );
+                            }),
+                      ),
+
+                      /// Blogs
+                      Container(
+                          padding: EdgeInsets.only(top: 16),
+                          child: ListView.builder(
+                            itemCount: articles.length,
+                            shrinkWrap: true,
+                            physics: ClampingScrollPhysics(),
+                            itemBuilder: (context, index) {
+                              return BlogTile(
+                                imageUrl: articles[index].urlToImage,
+                                title: articles[index].title,
+                                desc: articles[index].description,
+                              );
+                            },
+                          ))
+                    ],
+                  ),
+                ),
+              ));
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final imageUrl, categoryName;
+  CategoryTile({this.imageUrl, this.categoryName});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {},
+        child: Container(
+            margin: EdgeInsets.only(right: 16),
+            child: Stack(
+              children: <Widget>[
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(6),
+                    child: CachedNetworkImage(
+                      imageUrl: imageUrl,
+                      width: 120,
+                      height: 60,
+                      fit: BoxFit.cover,
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    width: 120,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: Colors.black26,
+                    ),
+                    child: Text(categoryName,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        )))
+              ],
+            )));
+  }
+}
+
+class BlogTile extends StatelessWidget {
+  final String imageUrl, title, desc;
+  BlogTile(
+      {@required this.imageUrl, @required this.title, @required this.desc});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+          margin: EdgeInsets.only(bottom: 16),
+          child: Column(
+            children: <Widget>[
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(6),
+                  child: Image.network(imageUrl)),
+              SizedBox(height: 8),
+              Text(title,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w500,
+                  )),
+              SizedBox(height: 8),
+              Text(desc, style: TextStyle(color: Colors.black54)),
+            ],
+          )),
+    );
+  }
+}
